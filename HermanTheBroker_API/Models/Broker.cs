2c4537502a465c1b0fd60c738c85e166e35051c2
Patrik Hermansson <hermansson.patrik@gmail.com>
2024-05-24 11:54:01 +0200

    Add project files.

---
+++ HermanTheBroker_API/Models/Broker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HermanTheBroker_API.Models
+{
+	public class Broker
+	{
+		[Key]
+		public int Id { get; set; }
+        public string Name { get; set; }
+		public string PhoneNumber { get; set; }
+		public virtual List<Residence> Residence { get; set; }
+
+	}
+}
